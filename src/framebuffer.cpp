7af4ede32d23efb0d40a80c8c6687773100e5fba
Noam Zeise <noam4134@gmail.com>
2023-06-07 15:16:24 +0100

    cleaned framebuffer creation code, more abstract renderbuffer create

--- src/framebuffer.cpp
+++ src/framebuffer.cpp
@@ -4,19 +4,10 @@
 #include <logger.h>
 #include "ogl_helper.h"
 
-GLuint genDepthStencilRenderBuffer(GLsizei width, GLsizei height, int samples) {
-    GLuint depth;
-    glGenRenderbuffers(1, &depth);
-    glBindRenderbuffer(GL_RENDERBUFFER, depth);
-    if(samples > 1)
-	glRenderbufferStorageMultisample(
-		GL_RENDERBUFFER, samples, GL_DEPTH24_STENCIL8, width, height);
-    else
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-    return depth;
-}
-  
+GLuint genRenderbuffer(int samples, int format, GLsizei width, GLsizei height);
+
+std::string framebufferError(int status);
+
 Framebuffer::Framebuffer(GLsizei width, GLsizei height, bool multisampling, bool depthBuffer) {
     glGenFramebuffers(1, &framebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
@@ -29,9 +20,7 @@ Framebuffer::Framebuffer(GLsizei width, GLsizei height, bool multisampling, bool
     }
 
     if(multisampling) {
-	glGenRenderbuffers(1, &colourBuff);
-	glBindRenderbuffer(GL_RENDERBUFFER, colourBuff);
-	glRenderbufferStorageMultisample(GL_RENDERBUFFER, maxSamples, GL_RGBA8, width, height);
+	colourBuff = genRenderbuffer(maxSamples, GL_RGBA8, width, height);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
 				  GL_RENDERBUFFER, colourBuff);
     } else {
@@ -44,34 +33,17 @@ Framebuffer::Framebuffer(GLsizei width, GLsizei height, bool multisampling, bool
 	glDrawBuffers(1, DrawBuffers);
     }
 
-    
     if(depthBuffer) {
-	depthStencilBuff = genDepthStencilRenderBuffer(width, height, maxSamples);
+	depthStencilBuff = genRenderbuffer(maxSamples, GL_DEPTH24_STENCIL8, width, height);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
 				  GL_RENDERBUFFER, depthStencilBuff);
     }
 
     int framebufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if(framebufferStatus != GL_FRAMEBUFFER_COMPLETE) {
-	//check which it matches
-	std::cout << "framebuffer error: ";
-	switch(framebufferStatus) {
-	case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-	    std::cout << "incomplete attachment";
-	    break;
-	case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-	    std::cout << "missing attachment";
-	    break;
-	case GL_FRAMEBUFFER_UNSUPPORTED:
-	    std::cout << "unsupported";
-	    break;
-	case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-	    std::cout << "multisampling incomplete";
-	    break;
-	}
-	std::cout << std::endl;
-	throw std::runtime_error("failed to create opengl framebuffer!");
-    }
+    if(framebufferStatus != GL_FRAMEBUFFER_COMPLETE)
+	throw std::runtime_error("failed to create opengl framebuffer! status: "
+				 + framebufferError(framebufferStatus));
+    
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     LOG("created framebuffer, width: " << width << "  height:" << height);
 }
@@ -88,4 +60,39 @@ GLuint Framebuffer::id() {
 
 GLuint Framebuffer::texture() {
     return texBuff;
+}
+
+GLuint genRenderbuffer(int samples, int format, GLsizei width, GLsizei height) {
+    GLuint rb;
+    glGenRenderbuffers(1, &rb);
+    glBindRenderbuffer(GL_RENDERBUFFER, rb);
+    if(samples > 1)
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, format, width, height);
+    else
+	glRenderbufferStorage(GL_RENDERBUFFER, format, width, height); 
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    return rb;
+}
+
+std::string framebufferError(int status) {
+    switch(status) {
+    case GL_FRAMEBUFFER_UNDEFINED:
+	return "undefined";
+    case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+	return "incomplete attachment";
+    case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+	return "missing attachment";
+    case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+	return "incomplete draw buffer";
+    case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+	return "incomplete read buffer";
+    case GL_FRAMEBUFFER_UNSUPPORTED:
+	return "unsupported";
+    case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+	return "multisampling incomplete";
+    case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
+	return "incomplete layer targets";
+    default:
+	return "unknown error code";
+    }
 }
